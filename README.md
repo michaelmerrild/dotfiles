e0c95fbd9bdd22733ad67b8a6703edb737d156b1
Michael Merrild <michael.merrild@gmail.com>
2022-04-17 19:45:38 +0700

    Add bootstrap

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dotfiles
+
+My personal dotfiles managed using [Chezmoi](https://www.chezmoi.io/).
+
+To bootstrap, run:
+
+```bash
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/michaelmerrild/dotfiles/main/bootstrap.zsh)"
+```
---
+++ bootstrap.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env zsh
+
+set -e
+
+if ! type brew &>/dev/null
+then
+  echo "Install Homebrew"
+  /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  echo "Homebrew already installed..."
+fi
+
+# Homebrew Bundle
+echo "Setup Chezmoi"
+brew install chezmoi
+chezmoi init --apply --verbose https://github.com/michaelmerrild/dotfiles.git
+
+echo "Install packages"
+brew bundle --file=$HOME/.config/brew/Brewfile --cleanup
+
+echo "Configure MacOS"
+$HOME/.config/macos/macos.zsh
+
+echo "Bootstrap completed"
