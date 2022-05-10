ca7512a31c26e31f7f6a97c688cc52c63e34be20
Michael Merrild <michael.merrild@gmail.com>
2022-05-10 09:13:31 +0700

    Update private_dot_config/zsh/env.zsh

--- private_dot_config/zsh/env.zsh
+++ private_dot_config/zsh/env.zsh
@@ -5,3 +5,5 @@ export GIT_CONFIG_GLOBAL="${HOME}/.config/git/config"
 # Path
 export PATH="${PATH}:${HOME}/.local/bin"
 export PATH="${PATH}:${HOME}/.fig/bin"
+export PATH="${PATH}:${HOME}/.rd/bin"
+
