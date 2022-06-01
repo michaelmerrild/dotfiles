35dd30a066aca864dbb33355ffd3e0123112c1bd
Michael Merrild <michael.merrild@gmail.com>
2022-06-01 22:16:23 +0700

    Update private_dot_config/zsh/env.zsh

--- private_dot_config/zsh/env.zsh
+++ private_dot_config/zsh/env.zsh
@@ -1,10 +1,12 @@
 
 export EDITOR="code --wait"
 export GIT_CONFIG_GLOBAL="${HOME}/.config/git/config"
+export PNPM_HOME="${HOME}/Library/pnpm"
 
 # Path
 export PATH="${PATH}:${HOME}/.local/bin"
 export PATH="${PATH}:${HOME}/.fig/bin"
 export PATH="${PATH}:${HOME}/.rd/bin"
+export PATH="${PATH}:${PNPM_HOME}"
 
 source "$ZDOTDIR/env.secret.zsh"
